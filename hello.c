ecf922a383ba155e4fb5111f8dbccda78a0cb07e
jitsukawa-hikaru <left.left2222@gmail.com>
2020-11-16 22:40:10 +0900

    first commit

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+<<<<<<< HEAD
+	printf("Hello World.\n");
+	puts("1");
+=======
+	printf("Hello World\n");
+	puts("10");
+>>>>>>> 98f50707527725013760e3ed3354f5c4da2e3672
+	return 0;
+}
+
